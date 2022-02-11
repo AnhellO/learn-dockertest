e5a16e44ffa80aa691dd119d3ab585d40a7e43a7
Angel Santiago Jaime Zavala <asjz2892@gmail.com>
2022-02-10 18:47:16 -0600

    feat: :sparkles: add first functional version of the seeded mongodb container

--- mongo/mongo_test.go
+++ mongo/mongo_test.go
@@ -3,6 +3,7 @@ package mongo_test
 import (
 	"context"
 	"fmt"
+	"log"
 	"testing"
 
 	"github.com/ory/dockertest/v3"
@@ -14,57 +15,77 @@ import (
 var dbClient *mongo.Client
 
 func TestMongo(t *testing.T) {
-	pool, err := dockertest.NewPool("")
+	network, err := pool.Client.CreateNetwork(docker.CreateNetworkOptions{Name: "mongo_network"})
 	if err != nil {
-		t.Fatalf("Could not connect to docker: %s", err)
+		log.Fatalf("could not create a network for mongo: %s", err)
 	}
 
-	// pull mongodb docker image for version 5.0
-	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
-		Repository: "mongo",
-		Tag:        "latest",
+	// First start the main mongodb container
+	mongodbResource, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository:   "mongo",
+		Tag:          "latest",
+		NetworkID:    network.ID,
+		Name:         "mongodb",
+		ExposedPorts: []string{"27017"},
 		Env: []string{
-			// username and password for mongodb superuser
-			"MONGO_INITDB_ROOT_USERNAME=root",
-			"MONGO_INITDB_ROOT_PASSWORD=password",
+			"MONGO_INITDB_ROOT_USERNAME=mongoadmin",
+			"MONGO_INITDB_ROOT_PASSWORD=secret",
+			"MONGO_INITDB_DATABASE=test_db",
 		},
 	}, func(config *docker.HostConfig) {
-		// set AutoRemove to true so that stopped container goes away by itself
 		config.AutoRemove = true
-		config.RestartPolicy = docker.RestartPolicy{
-			Name: "no",
-		}
+		config.RestartPolicy = docker.NeverRestart()
 	})
 	if err != nil {
-		t.Fatalf("Could not start resource: %s", err)
+		t.Fatalf("Could not start mongodb resource: %s", err)
 	}
 
-	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
-	err = pool.Retry(func() error {
+	// Exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+	if err = pool.Retry(func() error {
 		var err error
 		dbClient, err = mongo.Connect(
 			context.TODO(),
 			options.Client().ApplyURI(
-				fmt.Sprintf("mongodb://root:password@localhost:%s", resource.GetPort("27017/tcp")),
+				fmt.Sprintf("mongodb://mongoadmin:secret@localhost:%s/test_db?authSource=admin&readPreference=primary&directConnection=true&ssl=false", mongodbResource.GetPort("27017/tcp")),
 			),
 		)
 		if err != nil {
 			return err
 		}
 		return dbClient.Ping(context.TODO(), nil)
-	})
-
-	if err != nil {
+	}); err != nil {
 		t.Fatalf("Could not connect to docker: %s", err)
 	}
 
-	// When you're done, kill and remove the container
-	if err = pool.Purge(resource); err != nil {
-		t.Fatalf("Could not purge resource: %s", err)
+	// Finally build & start the mongoseeder container
+	mongoSeederResource, err := pool.BuildAndRunWithOptions("./seeder/Dockerfile", &dockertest.RunOptions{
+		NetworkID: network.ID,
+		Name:      "mongoseeder",
+	}, func(config *docker.HostConfig) {
+		config.AutoRemove = true
+		config.RestartPolicy = docker.NeverRestart()
+	})
+	if err != nil {
+		t.Fatalf("Could not start mongoseeder resource: %s", err)
 	}
 
-	// disconnect mongodb client
+	// When you're done, kill and remove the seeder first
+	if err = pool.Purge(mongoSeederResource); err != nil {
+		t.Fatalf("Could not purge mongoseeder resource: %s", err)
+	}
+
+	// Then remove the main mongodb container
+	if err = pool.Purge(mongodbResource); err != nil {
+		t.Fatalf("Could not purge mongodb resource: %s", err)
+	}
+
+	// Disconnect mongodb client
 	if err = dbClient.Disconnect(context.TODO()); err != nil {
 		panic(err)
+	}
+
+	// Finally remove the network
+	if err = pool.Client.RemoveNetwork(network.ID); err != nil {
+		log.Fatalf("could not remove %s network: %s", network.Name, err)
 	}
 }
